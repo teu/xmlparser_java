c5ba71b87c2b18673971ba6d89964fbe9d68fc37
teu <pjasiulewicz@gmail.com>
2015-12-12 13:33:09 +0100

    serving jsp

--- src/xmlparser/Books.java
+++ src/xmlparser/Books.java
@@ -1,6 +1,8 @@
 package xmlparser;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,21 +15,24 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/")
 public class Books extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Books() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private static final String listView = "/WEB-INF/book-list.jsp";
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public Books() {
+		super();
+		
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		RequestDispatcher distpatcher = request.getRequestDispatcher(listView);
+		distpatcher.forward(request, response);
 	}
 
 }
